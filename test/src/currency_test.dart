51200ef81ab65ba0f07130daa8f654491f3d0b8c
Marcelo Soares <marcelo.me.soares@gmail.com>
2024-11-02 22:39:58 -0300

    Add tests for `Currency` JSON serialization

--- test/src/currency_test.dart
+++ test/src/currency_test.dart
@@ -4,6 +4,7 @@
  * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
  */
 
+import 'dart:convert';
 import 'package:money2/money2.dart';
 import 'package:test/test.dart';
 
@@ -51,6 +52,64 @@ void main() {
       expect(Money.parseWithCurrency('1.1', t2).toString(), equals('₿1.1'));
       expect(Money.parseWithCurrency('1.11', t2).toString(), equals('₿1.11'));
       expect(Money.parseWithCurrency('1.01', t2).toString(), equals('₿1.01'));
+    });
+
+    group('JSON serialization', () {
+      final custom = Currency.create(
+        'USD',
+        2,
+        symbol: r'US$',
+        pattern: r'US$ 0.00',
+        groupSeparator: '_',
+        decimalSeparator: ',',
+        country: 'United States',
+        unit: 'dollar',
+        name: 'US Dollar',
+      );
+
+      final expectedJson = <String, dynamic>{
+        'isoCode': 'USD',
+        'decimalDigits': 2,
+        'symbol': r'US$',
+        'pattern': r'US$ 0.00',
+        'groupSeparator': '_',
+        'decimalSeparator': ',',
+        'country': 'United States',
+        'unit': 'dollar',
+        'name': 'US Dollar',
+      };
+
+      test('works correctly', () {
+        expect(custom.toJson(), equals(expectedJson));
+        expect(Currency.fromJson(expectedJson), equals(custom));
+        expect(Currency.fromJson(custom.toJson()), equals(custom));
+      });
+
+      test('encodes as string correctly', () {
+        final customJsonString = jsonEncode(custom);
+        final expectedJsonString = jsonEncode(expectedJson);
+        expect(customJsonString, equals(expectedJsonString));
+      });
+
+      test('decodes correctly from string', () {
+        final customJsonString = jsonEncode(custom);
+        final decodedCustom = Currency.fromJson(
+          jsonDecode(customJsonString) as Map<String, dynamic>,
+        );
+        expect(decodedCustom, equals(custom));
+      });
+
+      test('works both ways (serialization and deserialization)', () {
+        expect(custom, equals(Currency.fromJson(custom.toJson())));
+      });
+
+      test('full round trip is equal to original', () {
+        final customJsonString = jsonEncode(custom);
+        final decodedCustom = Currency.fromJson(
+          jsonDecode(customJsonString) as Map<String, dynamic>,
+        );
+        expect(decodedCustom, equals(custom));
+      });
     });
   });
 }
